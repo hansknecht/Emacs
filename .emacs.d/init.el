f2362c5f7a400ea42793353828aeb0ffdf351c5b
Hans Knecht <hknecht@hcg.com>
2023-09-19 10:16:10 -0700

    Initial commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,17 @@
+(string= (getenv "EMACS_PORTABLE") "Y")
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula-pro-pro)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4e92e26137642ca2fc40268c60b77ad77aa2d215c9638a3f7b5837103ff41b97" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ README.md
@@ -0,0 +1,13 @@
+# Portable Emacs
+I am making portable versions of the various editors. This is both a learning process as well as building out the tools that I need to do my development.
+
+Rebuilding enviornments is always a challenge.
+
+I carry these tools in a usb drive incase I do not have access.
+
+# Install
+1. Extract the repos
+1. Grab the version of Emacs you want to run from the gnu.org site http://ftp.gnu.org/gnu/emacs/windows/
+1. Extract the files into the emacs folder that was create in the first step
+1. update the runemacs.bat file if needed
+1. Double click runemacs.bat
